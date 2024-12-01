940d5940fae3b0b106142f20d3e555ec79783098
Arti554 <arthasani554@gmail.com>
2024-12-01 03:44:29 +0100

    java script comands on login

---
+++ scriptlog.js
@@ -0,0 +1,25 @@
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+
+    if (email === '' || password === '') {
+        alert('Ju lutem plotësoni të gjitha fushat për hyrje!');
+    } else {
+        alert(`Hyrja u krye me sukses! Mirë se vini, ${email}`);
+    }
+});
+
+
+document.getElementById('registerForm').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    const email = document.getElementById('registerEmail').value;
+    const password = document.getElementById('registerPassword').value;
+
+    if (email === '' || password === '') {
+        alert('Ju lutem plotësoni të gjitha fushat për regjistrim!');
+    } else {
+        alert(`Regjistrimi u krye me sukses! Mirë se vini, ${email}`);
+    }
+});
